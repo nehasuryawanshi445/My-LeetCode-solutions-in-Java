1b190a2fe1072e5c8fad2964d5c8634ad8a120ce
Neha Suryawanshi <nehasuryawanshi445@gmail.com>
2026-08-28 19:00:01 +0530

    Time: 1410 ms (8.7%), Space: 55.5 MB (66.47%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -2,7 +2,6 @@ class Solution {
     public int majorityElement(int[] nums) {
         int n = nums.length;
         int half = n / 2;
-        int majority = 0;
 
         for(int i = 0; i < n; i++){
             int cnt = 0;
